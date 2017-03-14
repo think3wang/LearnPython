e49aa5c7054ff58f361124fa2b56013ab3af71bd
Ziqiang Wang <ziqiang@eduroam-169-233-207-219.ucsc.edu>
2017-03-13 23:18:11 -0700

    end of learning on Mar. 13, 2017

---
+++ instance1.py
@@ -0,0 +1,15 @@
+import time
+import support
+import math
+
+
+# from fib import fibonacci
+
+
+
+ticks = time.time()
+print ticks
+support.print_func('ziqiang !')
+print 'dir of support', dir(support)
+print 'dir of ticks', dir(time)
+print 'dir of math', dir(math)
